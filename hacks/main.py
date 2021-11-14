96b1117338629c08799f8287603e31a7d3c2db71
Adityaaj54 <aditya.aj54@gmail.com>
2021-11-14 09:08:48 -0500

    updated script as

--- hacks/main.py
+++ hacks/main.py
@@ -2,12 +2,20 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routes import jobs,users,auth
-import azure.functions as func
-from .http_asgi import AsgiMiddleware
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(jobs.router)
 app.include_router(users.router)
@@ -17,5 +25,3 @@ app.include_router(auth.router)
 def root():
     return {"HELLO": "Welcome"}
 
-def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    return AsgiMiddleware(app).handle(req, context)
